0e2dd475fc56e448ccb90a2b556d57ade2b2ffbb
aravindanne <aravindannee007@gmail.com>
2026-02-09 12:13:46 +0530

    Refactor example_activity.go to use Greet function
    Removed SayHelloWorkflow and added Greet function.

--- internal/activities/example/example_activity.go
+++ internal/activities/example/example_activity.go
@@ -1,24 +1,10 @@
 package example
 
 import (
-	"time"
-
-	example "gosample/internal/activities/example"
-
-	"go.temporal.io/sdk/workflow"
+	"context"
+	"fmt"
 )
 
-func SayHelloWorkflow(ctx workflow.Context, name string) (string, error) {
-	ao := workflow.ActivityOptions{
-		StartToCloseTimeout: time.Second * 10,
-	}
-	ctx = workflow.WithActivityOptions(ctx, ao)
-
-	var result string
-	err := workflow.ExecuteActivity(ctx, example.Greet, name).Get(ctx, &result)
-	if err != nil {
-		return "", err
-	}
-
-	return result, nil
+func Greet(ctx context.Context, name string) (string, error) {
+	return fmt.Sprintf("Hello %s", name), nil
 }
